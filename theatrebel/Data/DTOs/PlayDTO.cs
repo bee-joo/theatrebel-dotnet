ce12542e4080cd64e90ce0fff185c62f149182f1
bee-joo <bukin-danila@yandex.ru>
2022-10-28 14:47:47 +0300

    Add Text field and error message

--- theatrebel/Data/DTOs/PlayDTO.cs
+++ theatrebel/Data/DTOs/PlayDTO.cs
@@ -1,15 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace theatrebel.Data.DTOs
 {
     public class PlayDTO
     {
-        [Required]
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; } = null!;
         public List<long> WriterIds { get; set; } = new List<long>();
         public string? Origname { get; set; }
         public int? Date { get; set; }
         public string? Description { get; set; }
-
+        public string? Text { get; set; }
     }
 }
