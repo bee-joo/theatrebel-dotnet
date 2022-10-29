a203e6effdc0544d0a28c244f820cdcf804f18a0
bee-joo <bukin-danila@yandex.ru>
2022-10-29 19:47:44 +0300

    Add play parameters object for query string

---
+++ theatrebel/Data/Parameters/PlayParameters.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace theatrebel.Data.Parameters
+{
+    public class PlayParameters : Parameters
+    {
+        public string? Name { get; set; }
+        public bool? HasText { get; set; }
+
+        [DefaultValue("Name")]
+        public new string? OrderBy { get; set; } = "Name";
+    }
+}
