1a1a0e1ce121e8b39a1fdc3aff41780ffcfc8f52
bee-joo <bukin-danila@yandex.ru>
2022-10-29 19:47:10 +0300

    Add Pagination object for query string

---
+++ theatrebel/Data/Parameters/Pagination.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace theatrebel.Data.Parameters
+{
+    public class Pagination 
+    {
+        [DefaultValue(1)]
+        public int Page { get; set; } = 1;
+        
+        [DefaultValue(20)]
+        public int Count { get; set; } = 20;
+    }
+}
