25002aa54fcae22a3766c910e53eb154eee078de
bee-joo <bukin-danila@yandex.ru>
2022-10-30 01:02:53 +0300

    Move base repo interfaces to Base folder

---
+++ theatrebel/Repositories/Interfaces/Base/IRepositoryBase.cs
@@ -0,0 +1,15 @@
+﻿namespace theatrebel.Repositories.Interfaces.Base
+{
+    public interface IRepositoryBase<T>
+    {
+        Task<T?> FindByIdAsync(long id);
+        Task<IList<T>> FindAllAsync();
+        Task<IList<T>?> FindAllByIdAsync(IList<long> ids);
+        IList<T> FindAllById(IList<long> ids);
+        T? Save(T entity);
+        Task<bool> ExistsByIdAsync(long id);
+        void SaveAll(IList<T> entities);
+        bool DeleteById(long id);
+        Task SaveChangesAsync();
+    }
+}
